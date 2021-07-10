6bde9dac079d157a7586f942aec181ff311d0a65
Sanchita Ghosh <sanchitag507@gmail.com>
2021-07-11 02:08:25 +0530

    changed headers part

--- server.js
+++ server.js
@@ -339,7 +339,7 @@ app.post('/forgot', function(req, res, next) {
                 subject: 'Reset your Password',
                 text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
                         'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-                        'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+                        'http://' + "myteams-video-chat.herokuapp.com" + '/reset/' + token + '\n\n' +
                         'If you did not request this, please ignore this email and your password will remain unchanged.\n'
             };
             smtpTransport.sendMail(mailOptions, function(err) {
